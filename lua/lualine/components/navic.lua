cc125627a321c1b18b49b729dfae2e25da31dc46
Smitesh Patil <smiteshpatil2010@gmail.com>
2023-04-21 21:34:15 +0530

    WIP

--- lua/lualine/components/navic.lua
+++ lua/lualine/components/navic.lua
@@ -1,6 +1,9 @@
 local navic = require("nvim-navic")
+local navic_lib = require("nvim-navic.lib")
 
 local M = require("lualine.component"):extend()
+local utils = require("lualine.utils.utils")
+local highlight = require("lualine.highlight")
 
 local default_options = {
 	cond = function()
@@ -8,12 +11,36 @@ local default_options = {
 	end
 }
 
+local function adjust_hl(section)
+	local lualine_bg = utils.extract_highlight_colors("lualine_" .. section .. highlight.get_mode_suffix(), "bg")
+	local lualine_fg = utils.extract_highlight_colors("lualine_" .. section .. highlight.get_mode_suffix(), "fg")
+
+	local text_hl = utils.extract_highlight_colors("NavicText")
+	if text_hl ~= nil and (text_hl.bg ~= lualine_bg or text_hl.fg ~= lualine_fg) then
+		highlight.highlight("NavicText", lualine_fg, lualine_bg)
+	end
+
+	local sep_hl = utils.extract_highlight_colors("NavicSeparator")
+	if sep_hl ~= nil and (sep_hl.bg ~= lualine_bg or sep_hl.fg ~= lualine_fg) then
+		highlight.highlight("NavicSeparator", lualine_fg, lualine_bg)
+	end
+
+	for i = 1, 26, 1 do
+		local hl_name = "NavicIcons"..navic_lib.adapt_lsp_num_to_str(i)
+		local hl = utils.extract_highlight_colors(hl_name)
+		if hl ~= nil and hl.bg ~= lualine_bg then
+			highlight.highlight(hl_name, hl.fg, lualine_bg)
+		end
+	end
+end
+
 M.init = function(self, options)
 	M.super.init(self, options)
 	self.options = vim.tbl_deep_extend("keep", self.options or {}, default_options)
 end
 
 M.update_status = function(self)
+	adjust_hl(self.options.self.section)
 	return navic.get_location()
 end
 
